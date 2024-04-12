c516b40e265257aa18a8c3f1baeb6ba30dd2657d
mr.root <1mr.rooter3@gmail.com>
2024-04-12 20:09:16 +0330

    add postCard

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,9 @@ module.exports = {
   content: ["./components/*.html"],
   theme: {
     fontFamily: {
-      'Roboto-thin': ['Roboto-thin'],
-      'Roboto-light': ['Roboto-light'],
-      'Roboto-md': ['Roboto-md'],
-      'Peyda': ['Peyda'],
+      "Roboto-light": ["Roboto-light"],
+      "Roboto-md": ["Roboto-md"],
+      Peyda: ["Peyda"],
     },
     container: {
       center: true,
